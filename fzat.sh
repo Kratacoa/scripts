23fab581452cdd5d7b479379e60f6c5c8971c6b5
Kratacoa <kratacoa@protonmail.com>
2020-08-11 17:05:53 +0200

    Now fzat uses find instead of TREE

--- fzat.sh
+++ fzat.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Fast searching through fzf for documents to input into a PDF editor
-
 # Set the environment variable for $READER if you do not want to use zathura
-reader=${READER:-zathura}
+# Set the environment variable for $DOCUMENTS_DIR if you do not want to use home
+# DEPENDENCIES: fzf
 
-tree -fiQ $HOME | fzf --multi --query=' .pdf | .djvu | .epub ' | setsid -f xargs "$reader" || echo "Something went wrong"
+reader=${READER:-zathura}
+dir=${DOCUMENTS_DIR:-$HOME}
+find $dir -regextype egrep -regex '.+pdf|.+djvu|.+epub' -printf "\"%p\"\n" | fzf --multi |  setsid -f xargs -r "$reader" || printf '\e[1;31m%s\e[0m' "Something went wrong"
