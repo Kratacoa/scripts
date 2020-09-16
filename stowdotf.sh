36e87fee20129afddb06065fbf92f09a2d4e058d
Kratacoa <kratacoa@protonmail.com>
2020-09-16 13:39:09 +0200

    stowdotf: fixed parameter substitution and folders list

--- stowdotf.sh
+++ stowdotf.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+# Stow all the files in the chosen directory, targetting the $HOME directory
+# DEPENDENCIES: stow (duh)
 
-dotfiles_dir=${$1:-"$HOME/dotfiles/config"}
+dotfiles_dir=${1:-"$HOME/dotfiles/config"}
 current_dir=$(pwd)
+list=$(find "$dotfiles_dir" -maxdepth 1 ! -path . -type d | sed '1d')
 
-list=$(find "$dotfiles_dir" -maxdepth 1 ! -path . -type d)
-cd $dotfiles_dir
-for i in $list; do
-    dir=$(basename $i)
-    stow -t "$HOME" -S "$dir"
-done
-cd "$current_dir"
+if cd "$dotfiles_dir"; then
+    for i in $list; do
+        dir=$(basename "$i")
+        stow --verbose --target="$HOME" -S "$dir"
+    done
+else
+	printf "%s\n" "Can't access the dotfiles directory"
+fi
+cd "$current_dir" ||
+printf "%s\n" "Couldn't return back to the current directory" && exit 1
